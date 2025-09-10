984e2ba9bdc8cb27a0acc32fd6c68561388c4119
Yonas <yonaslegesse19@gmail.com>
2025-09-10 11:21:14 +0300

    made otp fix

--- src/modules/otp/otp.module.ts
+++ src/modules/otp/otp.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { OtpService } from './otp.service';
 import { OtpController } from './otp.controller';
 import { SupabaseAuthService } from './supabase-auth.service';
@@ -7,7 +9,18 @@ import { DatabaseModule } from '../database/database.module';
 import { AuthPostgresService } from '../auth/auth-postgres.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_ACCESS_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('ACCESS_EXPIRES_IN') || '15m',
+        },
+      }),
+    }),
+  ],
   controllers: [OtpController],
   providers: [OtpService, SupabaseAuthService, AfroMessageService, AuthPostgresService],
   exports: [OtpService, SupabaseAuthService, AfroMessageService, AuthPostgresService],
