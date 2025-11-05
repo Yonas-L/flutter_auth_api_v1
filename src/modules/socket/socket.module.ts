06fa43d013cfb6784b8d9197452f490aca97e917
Yonas <yonaslegesse19@gmail.com>
2025-11-05 21:16:32 +0300

    new masive server 1

--- src/modules/socket/socket.module.ts
+++ src/modules/socket/socket.module.ts
@@ -1,16 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { SocketGateway } from './socket.gateway';
 import { AuthModule } from '../auth/auth.module';
 import { DatabaseModule } from '../database/database.module';
 import { DriverProfilesModule } from '../driver-profiles/driver-profiles.module';
+import { TripsModule } from '../trips/trips.module';
 
 @Module({
     imports: [
         AuthModule,
         DatabaseModule,
         DriverProfilesModule,
+        forwardRef(() => TripsModule), // Import TripsModule with forwardRef to resolve circular dependency
         JwtModule.registerAsync({
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => ({
--- src/modules/trips/trips.module.ts
+++ src/modules/trips/trips.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { TripsController } from './trips.controller';
@@ -8,7 +8,7 @@ import { DatabaseModule } from '../database/database.module';
 import { AuthPostgresModule } from '../auth/auth-postgres.module';
 import { DriverProfilesPostgresRepository } from '../database/repositories/driver-profiles-postgres.repository';
 import { VehiclesPostgresRepository } from '../database/repositories/vehicles-postgres.repository';
-import { SocketGateway } from '../socket/socket.gateway';
+import { SocketModule } from '../socket/socket.module';
 import { NotificationsModule } from '../../notifications/notifications.module';
 
 @Module({
@@ -16,6 +16,7 @@ import { NotificationsModule } from '../../notifications/notifications.module';
         DatabaseModule,
         NotificationsModule,
         AuthPostgresModule, // For JWT authentication
+        forwardRef(() => SocketModule), // Import SocketModule with forwardRef to resolve circular dependency
         JwtModule.registerAsync({
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => ({
@@ -32,7 +33,6 @@ import { NotificationsModule } from '../../notifications/notifications.module';
         TripStatusSyncService,
         DriverProfilesPostgresRepository,
         VehiclesPostgresRepository,
-        SocketGateway,
     ],
     exports: [TripsService],
 })
